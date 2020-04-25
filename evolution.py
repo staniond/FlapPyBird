240bbcfc0853c8a8aa13e3520060924cf0386a98
standa <ondra.stanicek@gmail.com>
2020-04-25 13:49:47 +0200

    changed printing evolution stats to better position to count overall best bird from current gen

--- evolution.py
+++ evolution.py
@@ -22,9 +22,6 @@ class Evolution:
         self.previous_population = self.previous_population + self.population
         self.population = []
 
-        print(f"Gen {self.generation_number}, "
-              f"best bird fitness: {self.pick_best_bird(self.previous_population).fitness}, "
-              f"overall best bird fitness: {self.best_fitness}")
         self.generation_number += 1
 
         prev_best_bird = self.pick_best_bird(self.previous_population)
@@ -32,6 +29,10 @@ class Evolution:
             self.best_fitness = prev_best_bird.fitness
             self.best_brain = prev_best_bird.brain.clone()
         self.population.append(Bird(self.best_brain.clone()))
+
+        print(f"Gen {self.generation_number}, "
+              f"best bird fitness: {self.pick_best_bird(self.previous_population).fitness}, "
+              f"overall best bird fitness: {self.best_fitness}")
 
         while len(self.population) < POPULATION_SIZE:
             first_brain, second_brain = self.pick_brains(self.previous_population)
